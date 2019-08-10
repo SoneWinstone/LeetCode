f98ac29f27ffe9a187bf55f63220a5fc20dc7c5c
SOneWinstone <jianwenzhen@qq.com>
2019-08-10 15:29:25 +0800

    [58].最后一个单词的长度

---
+++ 58.最后一个单词的长度.cpp
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode.cn id=58 lang=cpp
+ *
+ * [58] 最后一个单词的长度
+ *
+ * https://leetcode-cn.com/problems/length-of-last-word/description/
+ *
+ * algorithms
+ * Easy (30.61%)
+ * Likes:    125
+ * Dislikes: 0
+ * Total Accepted:    36.5K
+ * Total Submissions: 119K
+ * Testcase Example:  '"Hello World"'
+ *
+ * 给定一个仅包含大小写字母和空格 ' ' 的字符串，返回其最后一个单词的长度。
+ *
+ * 如果不存在最后一个单词，请返回 0 。
+ *
+ * 说明：一个单词是指由字母组成，但不包含任何空格的字符串。
+ *
+ * 示例:
+ *
+ * 输入: "Hello World"
+ * 输出: 5
+ *
+ *
+ */
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+  int lengthOfLastWord(string s) {
+    int ret = 0;
+    auto crb = s.crbegin();
+    for (; crb < s.crend(); ++crb) {
+      if (*crb == ' ') {
+        if (ret != 0) {
+          return ret;
+        }
+      } else {
+        ++ret;
+      }
+    }
+    return ret;
+  }
+};
