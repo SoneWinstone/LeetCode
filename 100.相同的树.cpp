b512ae894b8e25aa8d291279dc9301e70dad116e
SOneWinstone <jianwenzhen@qq.com>
2019-08-15 10:44:24 +0800

    [100].相同的树

---
+++ 100.相同的树.cpp
@@ -0,0 +1,76 @@
+/*
+ * @lc app=leetcode.cn id=100 lang=cpp
+ *
+ * [100] 相同的树
+ *
+ * https://leetcode-cn.com/problems/same-tree/description/
+ *
+ * algorithms
+ * Easy (54.05%)
+ * Likes:    216
+ * Dislikes: 0
+ * Total Accepted:    31.9K
+ * Total Submissions: 58.8K
+ * Testcase Example:  '[1,2,3]\n[1,2,3]'
+ *
+ * 给定两个二叉树，编写一个函数来检验它们是否相同。
+ *
+ * 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+ *
+ * 示例 1:
+ *
+ * 输入:       1         1
+ * ⁠         / \       / \
+ * ⁠        2   3     2   3
+ *
+ * ⁠       [1,2,3],   [1,2,3]
+ *
+ * 输出: true
+ *
+ * 示例 2:
+ *
+ * 输入:      1          1
+ * ⁠         /           \
+ * ⁠        2             2
+ *
+ * ⁠       [1,2],     [1,null,2]
+ *
+ * 输出: false
+ *
+ *
+ * 示例 3:
+ *
+ * 输入:       1         1
+ * ⁠         / \       / \
+ * ⁠        2   1     1   2
+ *
+ * ⁠       [1,2,1],   [1,1,2]
+ *
+ * 输出: false
+ *
+ *
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+  bool isSameTree(TreeNode *p, TreeNode *q) {
+
+    if (p == nullptr && q == p) {
+      return true;
+    } else if (q == nullptr || p == nullptr) {
+      return false;
+    }
+    if (p->val != q->val) {
+      return false;
+    }
+    return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+  }
+};
