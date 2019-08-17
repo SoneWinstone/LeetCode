44e2d23a6014a2d3283422fce8afd4931b2ee433
SOneWinstone <jianwenzhen@qq.com>
2019-08-17 09:29:05 +0800

    [110].平衡二叉树

---
+++ 110.平衡二叉树.cpp
@@ -0,0 +1,81 @@
+/*
+ * @lc app=leetcode.cn id=110 lang=cpp
+ *
+ * [110] 平衡二叉树
+ *
+ * https://leetcode-cn.com/problems/balanced-binary-tree/description/
+ *
+ * algorithms
+ * Easy (49.09%)
+ * Likes:    140
+ * Dislikes: 0
+ * Total Accepted:    25.5K
+ * Total Submissions: 51.9K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * 给定一个二叉树，判断它是否是高度平衡的二叉树。
+ * 
+ * 
+ * 本题中，一棵高度平衡二叉树定义为：
+ * 
+ * 
+ * 一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过1。
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 给定二叉树 [3,9,20,null,null,15,7]
+ * 
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ * 
+ * 返回 true 。
+ * 
+ * 示例 2:
+ * 
+ * 给定二叉树 [1,2,2,3,3,null,null,4,4]
+ * 
+ * ⁠      1
+ * ⁠     / \
+ * ⁠    2   2
+ * ⁠   / \
+ * ⁠  3   3
+ * ⁠ / \
+ * ⁠4   4
+ * 
+ * 
+ * 返回 false 。
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+    bool checkBalance(TreeNode *root, int& height) {
+        if (root == nullptr) {
+            return true;
+        }
+        int heightLeft = 0, heightRight = 0;
+        if (!checkBalance(root->left, heightLeft)) return false;
+        if (!checkBalance(root->right, heightRight)) return false;
+        if (abs(heightLeft - heightRight) > 1) {
+            return false;
+        }
+        height = 1 + max(heightLeft, heightRight);
+        return true;
+    }
+public:
+    bool isBalanced(TreeNode* root) {
+        int height = 0;
+        return checkBalance(root, height);
+    }
+};
